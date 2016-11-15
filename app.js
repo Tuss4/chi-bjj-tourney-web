8d5ab3e5887cea1d110d293d65640ce5bfad59cf
tuss4 <tuss4dbfn@gmail.com>
2016-11-15 00:24:26 -0600

    Some linting.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.use('/', express.static('public'))
+app.use('/', express.static('public'));
 
 app.listen(3000, function() {
-	console.log('Listening on port 3000!')
-})
+	console.log('Listening on port 3000!');
+});
