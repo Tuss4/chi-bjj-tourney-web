1daf88d35ec2c08221256afaa1e80736b14170eb
Tuss4 <tuss4dbfn@gmail.com>
2016-11-17 14:01:37 -0600

    Get rid of commented code.

--- public/js/web.js
+++ public/js/web.js
@@ -2,10 +2,6 @@ console.log("Mission control.");
 
 var loginUrl = 'https://api.tourneyfinder.com/v1/user/login';
 
-function reqListener () {
-    console.log(this.responseText);
-}
-
 var loginForm = new Vue({
     el: '#loginForm',
     data: {
@@ -16,7 +12,6 @@ var loginForm = new Vue({
         loginUser: function (e) {
             e.preventDefault();
             var xhr = new XMLHttpRequest();
-            // xhr.addEventListener("load", reqListener);
             xhr.open('POST', loginUrl);
             xhr.setRequestHeader("Content-Type", "application/json");
             xhr.onload = function () {
