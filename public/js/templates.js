fe666a2f3d27fb47ee71af69c90b0b6c35ab477f
Tuss4 <tuss4dbfn@gmail.com>
2016-11-18 15:06:26 -0600

    Started with the routes.

---
+++ public/js/templates.js
@@ -0,0 +1,36 @@
+var loginTemplate = `
+<div id=loginForm>
+    <form action='#'>
+        <div>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email">
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password">
+        </div>
+        <div>
+            <button type="submit">Login</button>
+        </div>
+    </form>
+</div>
+`;
+
+
+var registerTemplate = `
+<div id=registerForm>
+    <form action='#'>
+        <div>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email">
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password">
+        </div>
+        <div>
+            <button type="submit">Register</button>
+        </div>
+    </form>
+</div>
+`;
--- public/js/web.js
+++ public/js/web.js
@@ -2,45 +2,31 @@ console.log("Mission control.");
 
 var loginUrl = 'https://api.tourneyfinder.com/v1/user/login';
 
-// Set cookie.
-function setCookie (email, token) {
-    console.log(token);
-    document.cookie = "email=" + email;
-    document.cookie = "token=" + token;
+function addToStorage (email, token) {
+    localStorage.email = email;
+    localStorage.token = token;
 }
 
-function deleteCookie () {
-    document.cookie = "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+function clearStorage () {
+    localStorage.clear();
 }
 
-var cookieArray = document.cookie.split(';');
-var rawEmail = '';
-var userEmail = '';
-var rawToken = '';
-var userToken = '';
-if (cookieArray.length > 1) {
-  rawEmail = cookieArray[0];
-  userEmail = rawEmail.substring(6, rawEmail.length);
-  rawToken = cookieArray[1];
-  userToken = 'Token ' + rawToken.substring(6, rawToken.length);
-}
 
 var welcomeUser = new Vue({
     el: '#welcomeBack',
     data: {
-        email: userEmail, // Set this to whatever is in the cookie.
+        email: localStorage.getItem('email'), // Set this to whatever is in the cookie.
         seen: false
     },
     methods: {
       clearCookies: function (e) {
         e.preventDefault();
-        deleteCookie();
+        clearStorage();
         location.reload();
       }
   },
   created: function () {
-      if (rawEmail !== '') {
+      if (localStorage.getItem('email') !== null) {
           this.seen = true;
       }
   }
@@ -62,16 +48,34 @@ var loginForm = new Vue({
             xhr.onload = function () {
                 data = JSON.parse(xhr.responseText);
                 console.log(data);
-                setCookie(data.user.email, data.token);
+                addToStorage(data.user.email, data.token);
                 location.reload();
             };
             xhr.send(JSON.stringify({email:this.email, password:this.password}));
         }
     },
     created: function () {
-        if (rawEmail !== '') {
+        if (localStorage.getItem('email') !== null) {
             this.seen = false;
         }
     }
 });
-console.log(document.cookie);
+
+// Actual app
+
+const Login = { template: loginTemplate };
+const Register = { template: registerTemplate};
+
+
+const routes = [
+    { path: '/login', component: Login},
+    { path: '/register', component: Register}
+];
+
+const router = new VueRouter({
+    routes
+});
+
+const app = new Vue({
+    router
+}).$mount('#app');
