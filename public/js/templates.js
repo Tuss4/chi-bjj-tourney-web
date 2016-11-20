6f44b9c785e65a49dfe4d3b1a9b4f2359163d970
tuss4 <tuss4dbfn@gmail.com>
2016-11-19 23:35:34 -0600

    Troubleshooting the element not found issues.

--- public/js/templates.js
+++ public/js/templates.js
@@ -1,16 +1,17 @@
+/*jshint esversion: 6*/
 var loginTemplate = `
-<div id=loginForm>
+<div id='loginForm'>
     <form action='#'>
         <div>
             <label for="email">Email</label>
-            <input type="email" id="email" name="email">
+            <input type="email" id="email" name="email" v-model="email">
         </div>
         <div>
             <label for="password">Password</label>
-            <input type="password" id="password" name="password">
+            <input type="password" id="password" name="password" v-model="password">
         </div>
         <div>
-            <button type="submit">Login</button>
+            <button type="submit" v-on:click='loginUser'>Login</button>
         </div>
     </form>
 </div>
@@ -18,7 +19,7 @@ var loginTemplate = `
 
 
 var registerTemplate = `
-<div id=registerForm>
+<div id='registerForm'>
     <form action='#'>
         <div>
             <label for="email">Email</label>
--- public/js/web.js
+++ public/js/web.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6*/
 console.log("Mission control.");
 
 var loginUrl = 'https://api.tourneyfinder.com/v1/user/login';
@@ -10,56 +11,6 @@ function addToStorage (email, token) {
 function clearStorage () {
     localStorage.clear();
 }
-
-
-var welcomeUser = new Vue({
-    el: '#welcomeBack',
-    data: {
-        email: localStorage.getItem('email'), // Set this to whatever is in the cookie.
-        seen: false
-    },
-    methods: {
-      clearCookies: function (e) {
-        e.preventDefault();
-        clearStorage();
-        location.reload();
-      }
-  },
-  created: function () {
-      if (localStorage.getItem('email') !== null) {
-          this.seen = true;
-      }
-  }
-});
-
-var loginForm = new Vue({
-    el: '#loginForm',
-    data: {
-        email: '',
-        password: '',
-        seen: true
-    },
-    methods: {
-        loginUser: function (e) {
-            e.preventDefault();
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', loginUrl);
-            xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.onload = function () {
-                data = JSON.parse(xhr.responseText);
-                console.log(data);
-                addToStorage(data.user.email, data.token);
-                location.reload();
-            };
-            xhr.send(JSON.stringify({email:this.email, password:this.password}));
-        }
-    },
-    created: function () {
-        if (localStorage.getItem('email') !== null) {
-            this.seen = false;
-        }
-    }
-});
 
 // Actual app
 
@@ -79,3 +30,46 @@ const router = new VueRouter({
 const app = new Vue({
     router
 }).$mount('#app');
+
+var loginForm = new Vue({
+    el: '#loginForm',
+    data: {
+        email: '',
+        password: ''
+    },
+    methods: {
+        loginUser: function (e) {
+            e.preventDefault();
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', loginUrl);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.onload = function () {
+                data = JSON.parse(xhr.responseText);
+                console.log(data);
+                addToStorage(data.user.email, data.token);
+                location.reload();
+            };
+            xhr.send(JSON.stringify({email:this.email, password:this.password}));
+        }
+    }
+});
+
+// var welcomeUser = new Vue({
+//     el: '#welcomeBack',
+//     data: {
+//         email: localStorage.getItem('email'), // Set this to whatever is in the cookie.
+//         seen: false
+//     },
+//     methods: {
+//       clearCookies: function (e) {
+//         e.preventDefault();
+//         clearStorage();
+//         location.reload();
+//       }
+//   },
+//   created: function () {
+//       if (localStorage.getItem('email') !== null) {
+//           this.seen = true;
+//       }
+//   }
+// });
