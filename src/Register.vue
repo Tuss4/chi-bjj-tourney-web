857cb0df7c4e6059c48d00525a7ff667361b7ba6
Tuss4 <tuss4dbfn@gmail.com>
2016-11-22 15:11:52 -0600

    Some tweaks. Add a jshintrc

---
+++ src/Register.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="register">
+        <form action="#">
+            <div>
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" v-model="email">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" v-model="password">
+            </div>
+            <div>
+                <button type="submit">Register</button
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'register',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from './Login.vue'
+import Register from './Register.vue'
 
 
 Vue.use(VueRouter)
 
-new Vue({
-  el: '#app',
-  render: h => h(Login)
+const Home = { template: '<div>Events and stuff</div>'}
+
+
+const routes = [
+  { path: '/', component: Home},
+  { path: '/login', component: Login },
+  { path: '/register', component: Register }
+]
+
+const router = new VueRouter({
+  routes
 })
+
+const app = new Vue({
+  router
+}).$mount('#app')
