55dded9450e7c99ea8239670f1b95aa14d1f366e
Tuss4 <tuss4dbfn@gmail.com>
2016-11-21 21:58:41 -0600

    Project structure

---
+++ src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="login">
+        <form action="#">
+            <div>
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" v-model="email">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" v-model="password">
+            </div>
+            <div>
+                <button type="submit" v-on:click="loginUser">Login</button
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+const request = require('request')
+
+export default {
+  name: 'login',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    loginUser: function (e) {
+      e.preventDefault()
+      var body = JSON.stringify({email: this.email, password: this.password})
+      var url = 'https://api.tourneyfinder.com/v1/user/login'
+      var req = request.post(url, body)
+      console.log(req)
+    }
+  }
+}
+</script>
