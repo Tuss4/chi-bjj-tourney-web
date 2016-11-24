4caaa7249675444c74da3015de022dd86a1e2a90
tuss4 <tuss4dbfn@gmail.com>
2016-11-24 16:18:16 -0600

    Add confirm route

---
+++ src/Confirm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="confirm">
+    <p>{{ status }}</p>
+  </div>
+</template>
+
+<script>
+  const request = require('request')
+
+  export default {
+    name: 'confirm',
+    data () {
+      return {
+        status: ''
+      }
+    },
+    mounted: function () {
+      var url = 'https://api.tourneyfinder.com/v1/confirm/' + this.$route.params.token
+      console.log(url)
+      request({
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+        uri: url
+      }, function (error, response, body) {
+        console.log(response.statusCode)
+        var data = JSON.parse(body)
+        console.log(data)
+        console.log(this.status)
+      })
+    }
+  }
+</script>
