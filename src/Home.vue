88c1328ae9e3d9d056ad20e459647cecb88bf9fe
Tuss4 <tuss4dbfn@gmail.com>
2016-11-28 22:55:15 -0600

    Got the events to load up in the home vue!!

--- src/Home.vue
+++ src/Home.vue
@@ -2,18 +2,25 @@
     <div class="home">
         <p v-if="display">Welcome back, {{ email }} (<a href="#" v-on:click="logoutUser">logout</a>)!</p>
         <p>
-            Events and stuff.
+            <ul>
+                <li v-for="event in events">
+                    {{ event.name }} - {{ event.website }}
+                </li>
+            </ul>
         </p>
     </div>
 </template>
 
 <script>
+const request = require('request')
+
 export default {
   name: 'home',
   data () {
     return {
       email: '',
-      display: false
+      display: false,
+      events: []
     }
   },
   methods: {
@@ -21,9 +28,25 @@ export default {
       e.preventDefault()
       localStorage.clear()
       location.reload()
+    },
+    fetchEvents: function () {
+        var that = this
+        var url = 'https://api.tourneyfinder.com/v1/event'
+        request({
+            headers: {'Content-Type': 'application/json'},
+            uri: url
+        }, function (error, response, body) {
+            if (response.statusCode != 200) {
+                console.log(body)
+            } else {
+                var data = JSON.parse(body)
+                that.events = data.results
+            }
+        })
     }
   },
   mounted: function () {
+      this.fetchEvents()
       if (localStorage.getItem('email') && localStorage.getItem('token')) {
         this.email = localStorage.email
         this.display = true
